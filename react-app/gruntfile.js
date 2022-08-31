bd0b6bc478c0b8ae018f1c572e5cd8eb2baaad27
Daniel <morohoschidanieliosif@gmail.com>
2022-08-31 17:27:39 +0300

    added Gruntfile script for clean task and copy task

--- react-app/gruntfile.js
+++ react-app/gruntfile.js
@@ -1,11 +1,25 @@
-grunt.initConfig({
-  move: {
-    test: {
-      src: "old",
-      dest: "new",
+module.exports = function (grunt) {
+  grunt.initConfig({
+    move: {
+      move_deploy: {
+        src: "./build",
+        dest: "../server/views",
+      },
     },
-  },
-});
+    copy: {
+      copy_deploy: {
+        expand: true,
+        cwd: "./build/",
+        src: ["**"],
+        dest: "../server/views/",
+      },
+    },
+    clean: {
+      src: "../server/views/",
+    },
+  });
 
-grunt.loadNpmTasks("grunt-move");
-grunt.registerTask("default", ["move"]);
+  grunt.loadNpmTasks("grunt-move");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+};
